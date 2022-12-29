bdb383342542d3bbef8f9ed1df696e4bd0dd24d0
Ibtesamhaider <119958962+Ibtesamhaider@users.noreply.github.com>
2022-12-29 22:55:42 +0530

    Add files via upload

---
+++ 29-12-2022/Merge_Array.java
@@ -0,0 +1,27 @@
+//@Ibtesam Haider
+//Merge two array
+package thursday_lab1;
+
+public class Merge_Array {
+
+	public static void main(String[] args) {
+		int a[]= {1,2,3,4};
+		int b[]= {5,6,7,8};
+		int x=a.length,y=b.length;
+		int z=x+y;//new index c,which help to add a and b
+		int c[]=new int[z];//creating a new array c
+		for(int i=0;i<x;i++)
+		{
+			c[i]=a[i];//merging a in c
+		}
+		for(int i=0;i<y;i++)
+		{
+			c[x+i]=b[i];
+		}
+		for(int i=0;i<z;i++)
+		{
+		System.out.print(c[i]+" ");
+		}
+	}
+
+}
